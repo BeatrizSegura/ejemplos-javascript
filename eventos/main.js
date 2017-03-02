a32f9ef9aa0fe89cd353613c6ab01d13127bd4cb
BeatrizSegura <betty12_19@hotmail.com>
2017-03-02 13:00:13 -0600

    Ejemplos Eventos

---
+++ eventos/main.js
@@ -0,0 +1,16 @@
+function saludar()
+{
+    alert('Hola javascript')
+}
+
+function saludar2()
+{
+    document.write('Hola Documento')
+}
+
+var btnSaluda= document.getElementById("btn-saluda")
+btnSaluda.onclick =saludar
+
+var btnSaluda2= document.getElementById("btn-saluda-v2")
+btnSaluda2.addEventListener('click',saludar)
+btnSaluda2.addEventListener('click',saludar2)
